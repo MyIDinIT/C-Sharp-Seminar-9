949d07056f83b6d867d2ca7c4eca2a0ab1499ae2
IvanZH <ivzh.nn@gmail.com>
2023-03-20 21:49:24 +0300

    task64 completed

---
+++ task64/Program.cs
@@ -0,0 +1,13 @@
+﻿
+void Job(int n,int x=1)
+{
+    if (x > n) return;
+    else
+    Job(n,x+1);
+    Console.Write(x+" ");
+}
+
+Console.WriteLine("Ввеите числе:  ");
+int n=Convert.ToInt32(Console.ReadLine());
+
+Job(n);
